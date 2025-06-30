05b460b84cd1366a6bbc4e70b2484ff1cb026645
johnnyhelder <johnnyhelder@gmail.com>
2025-06-30 10:20:57 +0100

    Add vercel.json configuration and test page

---
+++ app/test-page/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div>
+      <h1>Test Page - Vercel Deploy</h1>
+      <p>If you see this, the deployment is working!</p>
+      <p>Time: {new Date().toISOString()}</p>
+    </div>
+  );
+}
